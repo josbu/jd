485a53866b379c1e6270a973f6b20310147d3868
oslook <oslook@163.com>
2021-07-14 02:55:29 +0800

    replace js path

--- onlyOneExecute.js
+++ onlyOneExecute.js
@@ -3,6 +3,13 @@ const fs = require("fs");
 const axios = require("axios");
 const smartReplace = require("./smartReplace");
 
+async function changeFiele() {
+    let response = await axios.get(process.env.SYNCURL);
+    let content = response.data;
+    content = await smartReplace.inject(content);
+    await fs.writeFileSync("./executeOnce.js", content, "utf8");
+    console.log("替换变量完毕");
+}
 
 async function start() {
     console.log(`北京时间 (UTC+08)：${new Date(new Date().getTime() + 8 * 60 * 60 * 1000).toLocaleString()}}`);
@@ -11,7 +18,17 @@ async function start() {
     } else {
         console.log("请填写 JD_COOKIE 后在继续");
     }
+    if (!process.env.SYNCURL) {
+        console.log("请填写 SYNCURL 后在继续");
+        return;
+    }
 
+    try {
+        await changeFiele();
+        await exec("node executeOnce.js", { stdio: "inherit" });
+    } catch (e) {
+        console.log("执行异常:" + e);
+    }
     console.log("执行完毕");
 }
 
